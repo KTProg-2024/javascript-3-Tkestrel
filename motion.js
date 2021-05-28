3dcc5f841be3ad3d5ab945ba82147eeb350a13d5
Toby Hansen <>
2021-05-28 13:35:01 +0000

    finished assignment

--- motion.js
+++ motion.js
@@ -1,14 +1,29 @@
-function myMove() {
-  var elem = document.getElementById("animate");   
-  var posTop = 0;
+function myMove1() {
+  var elem = document.getElementById("animate1");   
+  var posTop1 = 0;
   var posLeft = 0;
-  var id = setInterval(frame, 5);
+  var id1 = setInterval(frame, 5);
   function frame() {
-	  posTop++;
+	  posTop1++;
 	  posLeft++;
-	  elem.style.top = posTop + "px"; 
+	  elem.style.top = posTop1 + "px"; 
 	  elem.style.left = posLeft + "px"; 
-	  if ((posTop == 350) && (posLeft == 350))
-		  clearInterval(id);
+	  if ((posTop1 == 350) && (posLeft == 350))
+		  clearInterval(id1);
+  }
+}
+
+function myMove2() {
+  var elem2 = document.getElementById("animate2");   
+  var posTop2 = 0;
+  var posRight2 = 0;
+  var id2 = setInterval(frame, 5);
+  function frame() {
+	  posTop2++;
+	  posRight2++;
+	  elem2.style.top = posTop2 + "px"; 
+	  elem2.style.right = posRight2 + "px"; 
+	  if ((posTop2 == 350) && (posRight2 == 350))
+		  clearInterval(id2);
   }
 }
